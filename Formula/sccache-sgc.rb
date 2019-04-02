1f05ef0b1503b230bbaf511bc707a1ddd750b404
SygicCoreTechUnitBot <48092543+SygicCoreTechUnitBot@users.noreply.github.com>
2019-04-02 09:44:59 +0200

    Update sccache-sgc.rb

--- Formula/sccache-sgc.rb
+++ Formula/sccache-sgc.rb
@@ -1,15 +1,8 @@
-class Sccache < Formula
+class SccacheSgc < Formula
   desc "Used as a compiler wrapper and avoids compilation when possible"
   homepage "https://github.com/mozilla/sccache"
   url "https://github.com/mozilla/sccache/archive/0.2.7.tar.gz"
   sha256 "0f7e3ad60a93759a35623aa954633c94154fd00d17fe29f9413933e1c5545a52"
-  head "https://github.com/mozilla/sccache.git"
-
-  bottle do
-    sha256 "232e1af12684c02a086f8a937caae4140488153ff3d5be6ab2e30cd6aafdc3b4" => :mojave
-    sha256 "5142c49377699e069a1332b4fb681fef05fff6ad406ea1e0596786fd4f192e76" => :high_sierra
-    sha256 "e3687eef1ca2b74c95b48c2e3df2cc4d985afaf7fd454b9eab6b4837c1e9e24e" => :sierra
-  end
 
   depends_on "rust" => :build
   depends_on "openssl@1.1"
