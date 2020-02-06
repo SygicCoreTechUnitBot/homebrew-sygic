cc253e2fa13417e9ed259b94e31e54fc17740a81
SygicCoreTechUnitBot <48092543+SygicCoreTechUnitBot@users.noreply.github.com>
2020-02-06 15:28:16 +0100

    Add files via upload
    Add oclint-sgc

---
+++ Formula/oclint-sgc.rb
@@ -0,0 +1,23 @@
+class Oclint < Formula
+  desc "OCLint static code analysis tool for C, C++, and Objective-C"
+  homepage "http://oclint.org"
+  version '0.15'
+  sha256 'b6755a8a40cdc65bbf9a8d188909ad68906e62fe971c490f4dccbe4bb8a5d16d'
+  url "https://repository.sygic.com/repository/maven-sygic-3rdparty/org/oclint/oclint-sgc/#{version}/oclint-sgc-#{version}-x86_64-darwin18.7.0.tar.gz"
+  head "https://github.com/oclint/oclint.git"
+
+  def install
+    clang_version = '9.0.1'
+
+    include.install Dir['include/c++'] unless File.directory? "#{include}/c++"
+    "#{include}/c++".install Dir['include/c++/v1'] unless File.directory? "#{include}/c++/v1"
+    lib.install Dir['lib/clang'] unless File.directory? "#{lib}/clang"
+    "#{lib}/clang".install Dir['lib/clang/#{clang_version}'] unless File.directory? "#{lib}/clang/#{clang_version}"
+    lib.install Dir['lib/oclint']
+    bin.install Dir['bin/*']
+  end
+
+  test do
+    system "#{bin}/oclint", "-version"
+  end
+end
