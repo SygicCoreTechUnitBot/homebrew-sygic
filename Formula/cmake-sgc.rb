24ace8ef0ad4c54017451c501991f870bc6316b5
SygicCoreTechUnitBot <48092543+SygicCoreTechUnitBot@users.noreply.github.com>
2020-04-20 15:42:39 +0200

    Update cmake-sgc.rb

--- Formula/cmake-sgc.rb
+++ Formula/cmake-sgc.rb
@@ -1,8 +1,6 @@
 class CmakeSgc < Formula
   desc "Cross-platform make"
   homepage "https://www.cmake.org/"
-  url "https://github.com/Kitware/CMake/releases/download/v3.16.6/cmake-3.16.6.tar.gz"
-  sha256 "6f6ff1a197851b0fa8412ff5de602e6717a4eb9509b2c385b08589c4e7a16b62"
   head "https://gitlab.kitware.com/cmake/cmake.git"
 
   stable do
