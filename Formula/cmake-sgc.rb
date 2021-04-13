f69b2cfd962a80ac872bfbeba730e490550406de
SygicCoreTechUnitBot <48092543+SygicCoreTechUnitBot@users.noreply.github.com>
2021-04-13 12:25:16 +0200

    Update cmake-sgc.rb

--- Formula/cmake-sgc.rb
+++ Formula/cmake-sgc.rb
@@ -10,15 +10,15 @@ class CmakeSgc < Formula
     # Allows CMAKE_FIND_FRAMEWORKS to work with CMAKE_FRAMEWORK_PATH, which brew sets.
     # Remove with 3.18.0.
     patch do
-      url "https://gitlab.kitware.com/cmake/cmake/-/commit/c841d43d70036830c9fe16a6dbf1f28acf49d7e3.diff"
+      url "https://gitlab.kitware.com/cmake/cmake/-/commit/c841d43d70036830c9fe16a6dbf1f28acf49d7e3.patch"
       sha256 "87de737abaf5f8c071abc4a4ae2e9cccced6a9780f4066b32ce08a9bc5d8edd5"
     end
   end
 
   bottle do
-    root_url "https://repository.sygic.com/repository/raw-sygic-3rdparty/homebrew/bottles"
+    root_url "https://repository.sygic.com/repository/raw-sygic-3rdparty/homebrew/bottles/"
     sha256 cellar: :any_skip_relocation, high_sierra: "c8101e0b7b60fb28a9e07ee39e8f7bcaba1f21a387369e15f956d3ac25e4a29f"
-    sha256 cellar: :any_skip_relocation, catalina:    "e7ce33623794de90adfe90763720a256625b438d9665f06435651365713c3259" => :catalina
+    sha256 cellar: :any_skip_relocation, catalina:    "e7ce33623794de90adfe90763720a256625b438d9665f06435651365713c3259"
   end
   
   depends_on "sphinx-doc" => :build
